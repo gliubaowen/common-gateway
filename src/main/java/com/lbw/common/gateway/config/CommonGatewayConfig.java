e6a76f0adb35a0e176a61a4f2dbaa3c8f980b87b
BaoWenLiu <BaoWenLiu@DESKTOP-CIITADI>
2019-11-22 20:27:15 +0800

    修改网关配置为yaml方式

--- src/main/java/com/lbw/common/gateway/config/CommonGatewayConfig.java
+++ src/main/java/com/lbw/common/gateway/config/CommonGatewayConfig.java
@@ -3,8 +3,6 @@
  */
 package com.lbw.common.gateway.config;
 
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -40,27 +38,6 @@ public class CommonGatewayConfig {
 	@Bean
 	public FtpClientService ftpClientService() {
 		return new FtpClientService();
-	}
-
-	@Bean
-	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
-		return builder.routes()
-				.route(r -> r.path("/java/**").filters(f -> f.stripPrefix(1))
-						.uri("http://localhost:80/common-web/service/helloworld").order(1))
-				.route(r -> r.path("/baidu").uri("http://www.baidu.com"))
-				.route("path_route", r -> r.path("/get").uri("http://httpbin.org"))
-				.route("host_route", r -> r.host("*.myhost.org").uri("http://httpbin.org"))
-				.route("rewrite_route",
-						r -> r.host("*.rewrite.org").filters(f -> f.rewritePath("/foo/(?<segment>.*)", "/${segment}"))
-								.uri("http://httpbin.org"))
-				.route("hystrix_route",
-						r -> r.host("*.hystrix.org").filters(f -> f.hystrix(c -> c.setName("slowcmd")))
-								.uri("http://httpbin.org"))
-				.route("hystrix_fallback_route",
-						r -> r.host("*.hystrixfallback.org").filters(
-								f -> f.hystrix(c -> c.setName("slowcmd").setFallbackUri("forward:/hystrixfallback")))
-								.uri("http://httpbin.org"))
-				.build();
 	}
 
 }
--- src/main/java/com/lbw/common/gateway/controller/CommonGatewayController.java
+++ src/main/java/com/lbw/common/gateway/controller/CommonGatewayController.java
@@ -1,24 +1,24 @@
-/**
- * 
- */
-package com.lbw.common.gateway.controller;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-/**
- * Controller
- * 
- * @author LiuBaoWen
- *
- */
-@RestController
-@RequestMapping("service")
-public class CommonGatewayController {
-
-	@GetMapping("helloworld")
-	public String helloWorld() {
-		return "Hello Common Gateway!";
-	}
-}
+///**
+// * 
+// */
+//package com.lbw.common.gateway.controller;
+//
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+///**
+// * Controller
+// * 
+// * @author LiuBaoWen
+// *
+// */
+//@RestController
+//@RequestMapping("service")
+//public class CommonGatewayController {
+//
+//	@GetMapping("helloworld")
+//	public String helloWorld() {
+//		return "Hello Common Gateway!";
+//	}
+//}
